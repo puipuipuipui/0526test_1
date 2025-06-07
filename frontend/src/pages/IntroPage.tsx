7f85c82fa4770262c01ecec840b3a6921eaed42b
puipuipuipui <jessica910513@gmail.com>
2025-06-08 00:41:57 +0800

    修改文字

--- frontend/src/pages/IntroPage.tsx
+++ frontend/src/pages/IntroPage.tsx
@@ -75,7 +75,8 @@ function IntroPage({ onStart }: IntroPageProps) {
         
         <Title level={4} style={{ marginTop: '-10px', marginBottom: '-10px' }}>注意：</Title>
         <ul style={{ fontSize: '1.12rem', paddingLeft: '20px' }}>
-          <li style={{ marginBottom: '8px' }}>請把您的食指放在「E鍵和I鍵」上，以確保您能以最快反應進行測驗。</li>
+          <li style={{ marginBottom: '8px' }}>如果使用電腦，請把您的食指放在「E鍵和I鍵」上，以確保您能以最快反應進行測驗。</li>
+          <li style={{ marginBottom: '8px' }}>如果使用手機，請把您的拇指放在「螢幕左半邊和螢幕右半邊」上，以確保您能以最快反應進行測驗。</li>
           <li style={{ marginBottom: '8px' }}>上面的標題會告訴您哪一個字應該歸類到哪一個反應鍵。</li>
           <li style={{ marginBottom: '8px' }}>請盡可能快速並正確地分類每一項，太慢的分類會導致資料無效。</li>
           <li style={{ marginBottom: '8px' }}>如果您反應錯誤，會看到提示訊息，請修正答案。</li>
